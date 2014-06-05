c100dcdbd3b959b89bd26a6092a98adf177cfc35
kaustubh-d <kaustubh@clogeny.com>
2014-06-05 12:34:31 +0530

    unix only filter.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,12 @@ def windows?
   !!(RUBY_PLATFORM =~ /mswin|mingw|windows/)
 end
 
+def unix?
+  !windows?
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run_excluding :windows_only => true unless windows?
+  config.filter_run_excluding :unix_only => true unless unix?
 end
