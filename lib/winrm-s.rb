8cb6d27d173d0bd5e9d94d3e5671c3990680e73e
kaustubh-d <kaustubh@clogeny.com>
2014-06-05 12:33:27 +0530

    patch only on windows.

--- lib/winrm-s.rb
+++ lib/winrm-s.rb
@@ -1,3 +1,13 @@
 
 require 'winrm'
-require 'winrm/winrm_service_patch'
+
+def windows?
+  !!(RUBY_PLATFORM =~ /mswin|mingw|windows/)
+end
+
+# Monkey patch only on windows
+if windows?
+  require 'winrm/winrm_service_patch'
+else
+  puts "INFO: winrm-s does not monkey patch the winrm gem in *nix. You will continue to use winrm gem as is."
+end
