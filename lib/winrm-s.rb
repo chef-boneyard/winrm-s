e874a41d0960b584bb19ee21f7fe641564845a9f
adamedx <adamed@getchef.com>
2014-06-12 09:29:32 -0700

    Fix line endings, version, copyright notices, README update for release

--- lib/winrm-s.rb
+++ lib/winrm-s.rb
@@ -1,3 +1,20 @@
+#
+# Author:: Kaustubh Deorukhkar (<kaustubh@clogeny.com>)>
+# Copyright:: Copyright (c) 2014 Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 require 'winrm'
 
@@ -5,9 +22,9 @@ def windows?
   !!(RUBY_PLATFORM =~ /mswin|mingw|windows/)
 end
 
-# Monkey patch only on windows
+# Patch only on windows
 if windows?
   require 'winrm/winrm_service_patch'
 else
-  raise "ERROR: winrm-s does not monkey patch the winrm gem in *nix. You should use winrm gem on *nix."
+  raise "ERROR: winrm-s extensions to the winrm gem for the negotiate protocol are only supported on Windows. Require 'winrm' and not 'winrm-s' on non-Windows platforms."
 end
