bde8b249187bdb334fffe61f1aee22503f84775f
kaustubh-d <kaustubh@clogeny.com>
2014-07-04 13:10:11 +0530

    relax assertions and dont exit.

--- lib/winrm/helpers/assert_patch.rb
+++ lib/winrm/helpers/assert_patch.rb
@@ -6,15 +6,13 @@ module PatchAssertions
     loaded_ver = Gem.loaded_specs[library_name].version
     loaded_major_ver = loaded_ver.to_s.match(/(^\d+\.\d+)\.(.*)$/)[1]
     if loaded_major_ver.to_s != supported_major_ver.to_s
-      puts "Unsupported version of #{library_name}. The supported major version of library is #{library_name} version #{expected_major}. This code path monkey patches few methods in #{library_name} to support additional features. If you are aware of the impact of using #{loaded_ver}, this can be enabled by setting #{override_var_name} to the major version #{loaded_major_ver}"
-      exit 1
+      puts "WARNING: Unsupported version of #{library_name}. The supported major version of library is #{library_name} version #{expected_major}. This code path monkey patches few methods in #{library_name} to support additional features. This could possibly work, but it is advised to extensively test your version. If you are aware of the impact of using #{loaded_ver}, this warning can be disabled by setting #{override_var_name} to the major version #{loaded_major_ver}. "
     end
   end
 
   def self.assert_arity_of_patched_method(klass, method_name, expected_arity)
     if klass.instance_method(method_name).arity != expected_arity
-      puts "Cannot patch method #{klass}::#{method_name} since your latest gem seems to have different method definition that cannot be safely patched. Please use the supported version of patched gem."
-      exit 1
+      puts "WARNING: Cannot patch method #{klass}::#{method_name} since your latest gem seems to have different method definition that cannot be safely patched. This could possibly work, but it is advised to extensively test your version. Please use the supported version of patched gem."
     end
   end
 end
